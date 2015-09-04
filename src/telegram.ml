fdc48a1a536f6530a4d24478d5952384bc01940c
Evgenii Lepikhin <johnlepikhin@gmail.com>
2015-09-04 14:56:58 +0300

    Nethttp -> wget :(

--- src/telegram.ml
+++ src/telegram.ml
@@ -4,8 +4,15 @@ open BatPervasives
 let base_url = "https://api.telegram.org/bot"
 
 let perform pair fn params =
-	let client = Nethttp_client.Convenience.http_post_message (Printf.sprintf "%s%s/%s" base_url pair fn) params in
-	client#response_body#value
+	let open BatUnix in
+	let open Printf in
+	let params = List.map (fun (k,v) -> Netencoding.Url.(sprintf "%s=%s" (encode k) (encode v))) params |> String.concat "&" in
+	let ch_in = open_process_in (sprintf "/usr/bin/wget --no-check-certificate --post-data '%s' -qO - '%s%s/%s'" params base_url pair fn) in
+	let r = BatIO.read_all ch_in in
+	let res = close_process_in ch_in in
+	match res with
+		| WEXITED 0 -> r
+		| _ -> raise (failwith "wget error")
 
 let param k v = (k, v)
 
@@ -21,6 +28,3 @@ let sendMessage pair chat_id text =
 	let open Tel_ok_j in
 	let json = perform pair "sendMessage" [param "text" text; param "chat_id" chat_id] |> ok_of_string in
 	json.ok 
-
-let init () =
-	Nettls_gnutls.init ()
